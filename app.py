0e64016a4d2a5ea7af18d83c8cf13a6c06979c13
Dmitriy_Zakirov <metrio@inbox.ru>
2024-05-24 12:29:59 +0500

    added requirements

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 import os
 
 app = Flask(__name__, template_folder='templates')
-CORS(app) 
+CORS(app)
 
 UPLOAD_FOLDER = 'static'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
